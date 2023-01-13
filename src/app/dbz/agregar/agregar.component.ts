74a9867c168f542ac9d9c8d44a300ead991adbed
calegremc4 <calegre@mc4.com.bo>
2023-01-13 18:46:15 -0400

    fin de la seccion 5

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Personaje} from '../interfaces/dbz.interface';
+import {DbzService} from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css']
+})
+export class AgregarComponent{
+
+  @Input() nuevo: Personaje={ nombre: '', poder: 0};
+  // @Output() onNuevopersonaje: EventEmitter<Personaje> = new EventEmitter();
+  constructor(private dbzService: DbzService) {
+  }
+  agregar(){
+    if(this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+    this.dbzService.agregarPeronsaje(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
+  }
+}
